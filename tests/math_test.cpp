702e5a5a1879742ab11b7aea49daa4e8173953e3
nonyx777 <nahomretebo@gmail.com>
2024-01-08 09:18:02 +0300

    more tests on math util class

--- tests/math_test.cpp
+++ tests/math_test.cpp
@@ -16,8 +16,20 @@ TEST_CASE("Math Tests")
 
         float expected_deg = round(radian * Math::_rad2Deg());
         float expected_rad = degree * Math::_deg2Rad();
-        
+
         REQUIRE(expected_deg == degree);
         REQUIRE(expected_rad == Approx(radian));
+    }
+
+    SECTION("Vector operations")
+    {
+        sf::Vector2f vec1 = sf::Vector2f(0.f, 2.f);
+        sf::Vector2f vec2 = sf::Vector2f(1.f, 1.f);
+
+        REQUIRE(Math::_length(vec1) == 2);
+        REQUIRE(Math::_displacement(vec2, vec1) == sf::Vector2f(-1.f, 1.f));
+        REQUIRE(Math::_normalize(vec1) == sf::Vector2f(0.f, 1.f));
+        REQUIRE(Math::_dot(vec1, vec2) == 2.f);
+        REQUIRE(Math::_clampOnRange(4.f, 2.f, 3.f) == 3.f);
     }
 }
