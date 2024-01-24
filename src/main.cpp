769b58a898cf132aa14a28b2fa8f75010bc1ab6d
nonyx777 <nahomretebo@gmail.com>
2024-01-25 00:07:47 +0300

    main engine start right when config window ends

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,31 @@
 #include "../library/UI/imgui.h"
 #include "../library/UI/imgui-SFML.h"
 
+bool tempo = false;
+sf::Event event;
+
 int main()
 {
-    Engine engine;
 
     // Create a second SFML window for ImGui rendering
     sf::ContextSettings settings;
     sf::RenderWindow configWindow(sf::VideoMode(200, 200), "Config Window");
+    configWindow.setFramerateLimit(60);
     ImGui::SFML::Init(configWindow);
 
-    while (engine.running())
+    while (!tempo)
     {
-        engine.update();
+        while (configWindow.pollEvent(event))
+        {
+            ImGui::SFML::ProcessEvent(event);
+            switch (event.type)
+            {
+                case sf::Event::Closed:
+                    tempo = true;
+                    configWindow.close();
+                    break;
+            }
+        }
 
         // Render ImGui for the second SFML window (outside of the main SFML window)
         configWindow.clear();
@@ -23,8 +36,16 @@ int main()
         ImGui::End();
         ImGui::SFML::Render(configWindow);
         configWindow.display();
+    }
 
-        engine.render();
+    Engine engine;
+    if (tempo == true)
+    {
+        while (engine.running())
+        {
+            engine.update();
+            engine.render();
+        }
     }
 
     // Cleanup
